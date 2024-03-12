d906555ca3ad9fe783e06e7aba7afb1a5280d2cd
Jan1000dk <jan1000dk@gmail.com>
2024-03-12 09:53:28 +0100

    added TODOs for load and save CSV data as well edit some comments

--- Budget-Planner/src/main/java/CSVLoader.java
+++ Budget-Planner/src/main/java/CSVLoader.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CSVLoader {
-    public void loadData(String pathToFile) throws IOException, CsvValidationException {
+    public void displayData(String pathToFile) throws IOException, CsvValidationException {
         FileReader file = new FileReader(pathToFile);
         CSVReader csvReader = new CSVReader(file);
         String[] nextEntry;
@@ -13,29 +13,35 @@ public class CSVLoader {
         while ((nextEntry = csvReader.readNext()) != null) {
             //looping through the entire file and separates the coloums
             for (String cell : nextEntry) {
-                //Splits the cell-string to append an Amount coloum to the CLI table
+                //Splits the cell-string to append an Amount coloum to the CLI table & perform some light formatting
                 if (cell.contains("Expense Type")) {
                     String[] arrOfCell = cell.split(";", 2);
                     String cell1 = String.format("| %-25s", arrOfCell[0]);
                     String cell2 = String.format(" | %-6s |", "Amount");
                     System.out.printf(cell1 + cell2 + "\n");
+                    System.out.printf("______________________________________%n");
                 }
-                //Splits the cell-string to replace ; separator with =
+                //Splits the cell-string to replace ; separator with = & perform some light formatting
                 else if (!cell.contains("Income type")) {
                     String[] arrOfCell = cell.split(";", 2);
                     String cell1 = String.format("| %-25s", arrOfCell[0]);
                     String cell2 = String.format(" | %-6s |", arrOfCell[1]);
                     System.out.println(cell1 + cell2);
+                    System.out.printf("--------------------------------------%n");
                 }
-                //Splits the cell-string to replace ; with a space and |
+                //Splits the cell-string to replace ; with a space and | & perform some light formatting
                 else {
                     String[] arrOfCell = cell.split(";", 2);
                     String cell1 = String.format("| %-25s", arrOfCell[0]);
                     String cell2 = String.format(" | %-6s |", arrOfCell[1]);
                     System.out.println(cell1 + cell2);
+                    System.out.printf("______________________________________%n");
                 }
-                System.out.printf("--------------------------------------%n");
             }
         }
     }
+    //TODO - Should load all data from the CSV file and present them in a searchable manner
+    public String[] loadData(){return null;}
+    //TODO - Should use a CSVWriter to edit/replace data in the CSV file
+    public String[] saveData(){return null;}
 }
--- Budget-Planner/src/main/java/Main.java
+++ Budget-Planner/src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         CSVLoader csvLoader = new CSVLoader();
         try {
-            csvLoader.loadData(".\\CSVPrototype.csv");
+            csvLoader.displayData(".\\CSVPrototype.csv");
         } catch (IOException | CsvValidationException e) {
             throw new RuntimeException(e);
         }
